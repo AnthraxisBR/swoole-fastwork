ff59af996c5d36442a69cff7dd1f1b36e1801a4d
Gabriel Melo <gabrielmouraodemelo@gmail.com>
2019-05-25 22:45:12 -0300

    melhoria config

--- src/server/HttpServer.php
+++ src/server/HttpServer.php
@@ -25,6 +25,12 @@ class HttpServer extends \swoole_http_server
 
     public $app;
 
+    public $task_ipc_mode;
+
+    public $message_queue_key;
+
+    public $task_tmpdir;
+
     public function __construct(Application $app, array $config)
     {
         $this->host = $config['host'];
@@ -56,14 +62,24 @@ class HttpServer extends \swoole_http_server
         if($this->hasWorkerEnabled()){
             $this->worker_num = isset($this->config['worker']['worker_num']) ? $this->config['worker']['worker_num'] : 1;
             $this->task_worker_num = isset($this->config['worker']['task_worker_num']) ? $this->config['worker']['task_worker_num'] : 1;
+            $this->task_ipc_mode = isset($this->config['worker']['task_ipc_mode']) ? $this->config['worker']['task_ipc_mode'] : null;
+            $this->message_queue_key = isset($this->config['worker']['message_queue_key']) ? $this->config['worker']['message_queue_key'] : null;
+            $this->task_tmpdir = isset($this->config['worker']['task_tmpdir']) ? $this->config['worker']['task_tmpdir'] : null;
 
-            $this->set([
-                'worker_num' => 1,
-                'task_worker_num' => 1,
-                //'task_ipc_mode' => 3,
-                //'message_queue_key' => 0x70001001,
-                //'task_tmpdir' => '/data/task/',
-            ]);
+            $workers_config = [];
+            $workers_config['worker_num'] = $this->worker_num;
+            $workers_config['task_worker_num'] = $this->task_worker_num;
+
+            if(!is_null($this->task_ipc_mode)){
+                $workers_config['task_ipc_mode'] = $this->task_ipc_mode;
+            }
+            if(!is_null($this->message_queue_key)){
+                $workers_config['message_queue_key'] = $this->message_queue_key;
+            }
+            if(!is_null($this->task_tmpdir)){
+                $workers_config['task_tmpdirtask_worker_num'] = $this->task_tmpdir;
+            }
+            $this->set($workers_config);
 
 
             $this->on('Task', function (\swoole_server $serv, $task_id, $from_id, $data) {
@@ -71,6 +87,22 @@ class HttpServer extends \swoole_http_server
                 $this->app->runSignedTask($serv, $task_id, $from_id, $data);
                 $serv->finish($this->app->taskResponse);
                 echo 'concluído';
+            });
+
+            $this->on('workerStart', function(\swoole_server $serv, $worker_id) {
+                global $argv;
+                if ($serv->taskworker)
+                {
+                    \swoole_set_process_name("php {$argv[0]}: task_worker");
+                }
+                else
+                {
+                    \swoole_set_process_name("php {$argv[0]}: worker");
+                }
+            });
+            $this->on('workerStop', function (\swoole_server $serv, $id) {
+                echo "stop\n";
+                var_dump($id);
             });
         }
     }
